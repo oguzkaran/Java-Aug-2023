e732fe06f2ff6b3363b0d935a2a950fe0beb88aa
Oğuz Karan <oguzkaran@csystem.org>
2023-08-12 12:53:26 +0300

    Update

--- src/src-console/csd/App.java
+++ src/src-console/csd/App.java
@@ -1,8 +1,19 @@
 package csd;
 
 class App {
-	public static void main(String [] args)
+	public static void main(String [] args) 
 	{
-		System.out.println("Hello, World");
+		Util.foo();
 	}
+
+
+}
+
+
+class Util {
+	@Deprecated
+	public static void foo()
+	{
+		//...
+	}	
 }
