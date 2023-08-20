d5aebcb6ef5bf0a2f183df1c1d71329fd2955e3d
Oğuz Karan <oguzkaran@csystem.org>
2023-08-20 13:05:12 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,24 +1,29 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Değişken isimlendirme kuralları:
-	Bu kurallar Java'da tüm değişken atomlar için geçerlidir.
-	
-	- Değişken isimleri sayısal bir karakter ile başlatılamaz. Uygun bir karakter ile başlatılıp istenildiği kadar 
-	sayısal karakter kullanılabilir.
-	
-	- Değişken ismi alfabetik bir karakter ile başlatılabilir
-	
-	- Değişken isminde _ karakteri kullanılabilir. Hatta değişken ismi _ karakteri ile başlatılabilir.
-	
-	- Java 8 ile birlikte _ karakterinin tek başına kullanılması deprecated olmuştur. Java 9 ile birlikte de artık 
-	geçersiz duruma gelmiştir.
-	
-	- Anahtar sözcükler tek başına değişken ismi olarak kullanılamaz
+	Anahtar Notlar: Java derleyicisi akışın herhangi bir noktaya kesinlikle gelemeyeceğini anladığında genel olarak 
+	error oluşturur. Bu tip kod parçalarına "unreachable codes" da denilmektedir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
-class App {
-	public static void main(String [] args)
-	{		
-		int _;
+class App {	
+	public static void main(String [] args) 
+	{
+		int result;
+		
+		result = NumberUtil.add() * 2; 
+		
+		System.out.println(result);
+	}
+}
+
+class NumberUtil {
+	public static int add() //error
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.println("İki sayı giriniz:");
+		int a = kb.nextInt();
+		int b = kb.nextInt();		
+		
+		return a + b;
+		System.out.println("Unreachable code"); //error
 	}
 }
