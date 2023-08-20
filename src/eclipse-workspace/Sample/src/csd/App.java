44c52185e380aec713f00e065c4135abb64e0721
Oğuz Karan <oguzkaran@csystem.org>
2023-08-20 10:40:31 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,24 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örneği çalıştırıp yuvarlama hatasını gözlemleyiniz. Kodların detayları şu an önemsizdir. Yalnızca yuvarlama
-	hatasına odaklanınız	
+	Anahtar Notlar: Aslında ekrana basmak ve klavyeden okuma kavramları JLS'de doğrudan bu şekilde belirtilmemiştir.
+	print ve println metotları aslında "standard output (stdout)" denilen bir dosyaya yazma yaparlar. Bu anlamda klavyeden 
+	bilgi okuyan metotlar da "standard input (stdin)" denilen bir dosyadan okuma yaparlar. Ancak, bu dosyalar çeşitli
+	sistemlerde belli aygıtlara yönlendirilmiştir. Örneğin klasik masaüstü işletim sistemlerinde tipik olarak stdout ekrana,
+	stdin de klavyeye yönlendirilmiştir. Biz kursumuzda ekran ve kalvye terimlerini kullanacağız
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{
-		double a = 0.1, b = 0.2;
-		double c;
+	{		
 		
-		c = a + b;
-		
-		System.out.printf("a = %.20f%n", a);
-		System.out.printf("b = %.20f%n", b);		
-		System.out.printf("c = %.20f%n", c);
-		
-		if (c == 0.3)
-			System.out.println("Eşit");
-		else
-			System.out.println("Eşit değil");
 	}
 }
