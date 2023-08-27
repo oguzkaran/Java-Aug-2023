7eadc26597645b60811602128f62aef824ef01f6
Oğuz Karan <oguzkaran@csystem.org>
2023-08-27 15:56:43 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,15 +1,13 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	JavaSE'de de bir çok sınıf ve bir çok metot vardır. Ayrıca programcı geliştirme sırasında kullandığı kütüphane ve 
-	ortamlarda da (framework) pek çok sınıf ve metot bulunur. Peki programcı bir konuya ilişkin metodu ne zaman 
-	yazmaya karar vermelidir? Yani başka bir deyişle JavaSE ya da diğer kullanılan ortam ve kütüphanelerde ilgili
-	işlemi yapan metot ya da metotlar varsa (burada ileride sınıf olarak da düşüneceğiz) kendisi yazmalı mıdır? 
+	Math sınıfının PI ve E veri elemanları
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		
+		System.out.println(Math.PI);
+		System.out.println(Math.E);
 	}
 }
 
