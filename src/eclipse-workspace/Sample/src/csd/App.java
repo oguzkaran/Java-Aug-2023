aaa248e3763a84af1822dd34ceae34958d2eb44e
Oğuz Karan <oguzkaran@csystem.org>
2023-08-27 13:11:59 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,24 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Klavyeden girilen iki tam sayının toplamını, çarpımını ve farkını açıklamalara göre ekrana bastıran
-	programı yazınız: 
-	Açıklamalar:
-		- Girilen sayılar 10 ve 20 olsun. Ekran çıktısı şu şekilde olacaktır:
-		  	10 + 20 = 30
-		  	10 * 20 = 200
-		  	10 - 20 = -10
-		- Ekrana formatlı yazdırmak için printf metodunu kullanınız 
+	JavaSE'de de bir çok sınıf ve bir çok metot vardır. Ayrıca programcı geliştirme sırasında kullandığı kütüphane ve 
+	ortamlarda da (framework) pek çok sınıf ve metot bulunur. Peki programcı bir konuya ilişkin metodu ne zaman 
+	yazmaya karar vermelidir? Yani başka bir deyişle JavaSE ya da diğer kullanılan ortam ve kütüphanelerde ilgili
+	işlemi yapan metot ya da metotlar varsa (burada ileride sınıf olarak da düşüneceğiz) kendisi yazmalı mıdır? 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		System.out.print("İki sayı giriniz:");
-		int a = kb.nextInt();
-		double b = kb.nextDouble();
-		
-		System.out.format("a = %d, b = %.20f%n", a, b);
 	}
 }
+
