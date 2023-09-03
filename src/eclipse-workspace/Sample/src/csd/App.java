98ce5ae0e59712684dd55ab9b9d66a1f4c646e23
Oğuz Karan <oguzkaran@csystem.org>
2023-09-03 21:45:49 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,14 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Bir karakterin sıra numası ters bölü ve u ile birlikte hexadecimal olarak da verilebilmektedir. Bu durumda ilgili
-	karakterin sıra numarası alınmış olur
+	++ operatörü nasıl kullanılırsa kullanılsın operandına ilişkin değeri 1 artırır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		char c = '\u04f4';
 		
-		System.out.println(c);
 	}
 }
