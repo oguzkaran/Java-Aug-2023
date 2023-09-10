c4d2cd6deb71bcf2434279a385abbbe132708d2c
Oğuz Karan <oguzkaran@csystem.org>
2023-09-10 12:54:07 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,34 +1,53 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	 Aşağıdaki örneği inceleyiniz 
+	 Sınıf Çalışması: Klavyeden katsayıları girilen ikinci dereceden bir denklemin köklerini bulan programı yazınız
+	 Not: İleride daha iyisi yazılacaktır 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.print("Bir sayı giriniz:");
-		int a = kb.nextInt();
-		
-		if (!Util.isOdd(a))
-			System.out.printf("Çift sayı girdiniz:%d%n", a);
-		else
-			System.out.printf("Tek sayı girdiniz:%d%n", a);
-		
-		System.out.println("Tekrar yapıyor musunuz?");
+		QuadraticEquationApp.run();		
 	}
 }
 
-class Util {
-	public static boolean isEven(int a)
+class QuadraticEquationApp {
+	public static void run()
 	{
-		return a % 2 == 0;		
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Kaysayıları giriniz:");
+		double a = kb.nextDouble();
+		double b = kb.nextDouble();
+		double c = kb.nextDouble();
+		
+		QuadraticEquation.findRoots(a, b, c);		
+	}
+}
+
+class QuadraticEquation {
+	public static double calculateDelta(double a, double b, double c)
+	{
+		return b * b - 4  * a * c;
 	}
 	
-	public static boolean isOdd(int a)
+	public static void findRoots(double a, double b, double c)
 	{
-		return !isEven(a);
+		double delta = calculateDelta(a, b, c);
+		
+		if (delta >= 0) 
+			printRoots(a, b, delta);		
+		else
+			System.out.println("Gerçek kök yok");		
 	}
+	
+	public static void printRoots(double a, double b, double delta)
+	{
+		double sqrtDelta = Math.sqrt(delta);
+		double x1 = (-b + sqrtDelta) / (2 * a);
+		double x2 = (-b - sqrtDelta) / (2 * a);
+		
+		System.out.printf("x1 = %f, x2 = %f%n", x1, x2);
+		
+	}	
 }
 
