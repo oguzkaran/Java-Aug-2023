cb172f7d33ee6705ef859df606a35e0ef9b8396e
Oğuz Karan <oguzkaran@csystem.org>
2023-09-24 12:47:44 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,42 +1,27 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı bir n değeri için n-inci Fibonacci sayısına geri dönen nthFibonacciNumber isimli
-	metodu NumberUtil sınıfı içerisinde aşağıdaki açıklamalara göre yazınız ve aşağıdaki kod ile test ediniz
-	Açıklamalar:
-		- n değerinin pozitif olup olmadığı kontrolü yapılmayacaktır
-		- Fibonacci sayılarına ilişkin seri şu şekildedir
-			0 1 1 2 3 5 8 13 21 ...	
+	switch deyimi: 	switch belirli koşullar altında sabit olan değerleri eşitlik karşılaştırmasına sokmak ve duruma göre
+	işlem yapmak için kullanılan bir kontrol deyimidir. Yani if deyimine alternatif olmasa da belirli koşullar altında
+	if deyimi yerine kullanılabilen bir deyimdir. switch deyimi okunabilirliği artırmak için if deyimi yerine tercih
+	edilebilir. switch deyiminin geneş biçimi şu şekildedir:
+		switch (<ifade>) {
+			case <sabit ifadesi>:
+				<deyim>
+			case <sabit ifadesi>:
+				<deyim>
+				
+				...
+			[
+			default:
+				<deyim>
+			]
+		}
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		NumberUtilNthFibonacciNumberTest.run();
+		
 	}
 }
 
-class NumberUtilNthFibonacciNumberTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		while (true) {
-			System.out.print("Bir sayı giriniz:");
-			int n = Integer.parseInt(kb.nextLine());
-			
-			if (n <= 0)
-				break;
-			
-			System.out.printf("%d.Fibonacci sayısı:%d%n", n, NumberUtil.nthFibonacciNumber(n));
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-class NumberUtil {
-	public static int nthFibonacciNumber(int n)
-	{
-		//TODO:
-	}
-}
