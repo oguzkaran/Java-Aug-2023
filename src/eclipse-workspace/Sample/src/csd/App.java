743f312945dde4dc34f6aba70b0ad6cd80ba2d3e
Oğuz Karan <oguzkaran@csystem.org>
2023-09-24 10:28:50 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,63 +1,26 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı bir sayıdan küçük en büyük asal sayıya geri dönen previousClosestPrime isimli metodu 
-	yazınız ve aşağıdaki kod ile test ediniz
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{
-		NumberUtilPreviousClosestPrimeTest.run();
+	{		
+		EXIT_LOOP:
+		for (int i = 2; i <= 20; ++i) 
+			EXIT_INNER_LOOP:
+			for (int j = 3; j <= 40; ++j) 
+				for (int k = 30; k >= 3; --k) {
+					System.out.printf("(%d, %d, %d)%n", i, j, k);
+					
+					if ((i +  j + k) % 11 == 0)
+						break EXIT_LOOP;
+					
+					if ((i +  j + k) % 7 == 0)
+						break EXIT_INNER_LOOP;
+				}	
+					
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
-
-class NumberUtilPreviousClosestPrimeTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		for (;;) {
-			System.out.print("Bir sayı giriniz:");
-			long n = Long.parseLong(kb.nextLine());		
-			
-			
-			System.out.printf("%d. sayısından küçük en büyük asal sayı:%d%n", n, NumberUtil.previousClosestPrime(n));
-			
-			if (n == 0)
-				return;
-		}
-	}
-}
-
-class NumberUtil {
-	public static long previousClosestPrime(long val)
-	{
-		//TODO:				
-	}
-	
-	public static boolean isPrime(long val)
-	{
-		if (val <= 1)
-			return false;
-		
-		if (val % 2 == 0)
-			return val == 2;
-		
-		if (val % 3 == 0)
-			return val == 3;
-		
-		if (val % 5 == 0)
-			return val == 5;
-		
-		if (val % 7 == 0)
-			return val == 7;
-	
-		for (long i = 11; i * i <= val; i += 2)		
-			if (val % i == 0)
-				return false;
-		
-		return true;		
-	}
-}
-
 
