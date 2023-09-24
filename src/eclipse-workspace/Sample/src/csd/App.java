3fc527249cd6a4e9b549cf31680359d20e1d6bc4
Oğuz Karan <oguzkaran@csystem.org>
2023-09-24 11:36:55 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,26 +1,42 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	
+	Sınıf Çalışması: Parametresi ile aldığı bir n değeri için n-inci Fibonacci sayısına geri dönen nthFibonacciNumber isimli
+	metodu NumberUtil sınıfı içerisinde aşağıdaki açıklamalara göre yazınız ve aşağıdaki kod ile test ediniz
+	Açıklamalar:
+		- n değerinin pozitif olup olmadığı kontrolü yapılmayacaktır
+		- Fibonacci sayılarına ilişkin seri şu şekildedir
+			0 1 1 2 3 5 8 13 21 ...	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		EXIT_LOOP:
-		for (int i = 2; i <= 20; ++i) 
-			EXIT_INNER_LOOP:
-			for (int j = 3; j <= 40; ++j) 
-				for (int k = 30; k >= 3; --k) {
-					System.out.printf("(%d, %d, %d)%n", i, j, k);
-					
-					if ((i +  j + k) % 11 == 0)
-						break EXIT_LOOP;
-					
-					if ((i +  j + k) % 7 == 0)
-						break EXIT_INNER_LOOP;
-				}	
-					
+		NumberUtilNthFibonacciNumberTest.run();
+	}
+}
+
+class NumberUtilNthFibonacciNumberTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		while (true) {
+			System.out.print("Bir sayı giriniz:");
+			int n = Integer.parseInt(kb.nextLine());
+			
+			if (n <= 0)
+				break;
+			
+			System.out.printf("%d.Fibonacci sayısı:%d%n", n, NumberUtil.nthFibonacciNumber(n));
+		}
+		
 		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
+class NumberUtil {
+	public static int nthFibonacciNumber(int n)
+	{
+		//TODO:
+	}
+}
