ebd12623e78c4694dadfa7c5c67d458765691293
Oğuz Karan <oguzkaran@csystem.org>
2023-10-14 11:13:32 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,12 +1,20 @@
-/*----------------------------------------------------------------------------------------------------------------------
-	 
+/*----------------------------------------------------------------------------------------------------------------------	 
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
-	public static void main(String [] args) 
-	{		
+	public static void main(String [] args)
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		System.out.print("İki sayı giriniz:");
+		double a = kb.nextDouble();
+		double b = kb.nextDouble();
+		double c;
+		
+		c = Math.sqrt(a) + Math.round(b);
+	
+		System.out.printf("c = %f%n", c);
 	}
 }
-
