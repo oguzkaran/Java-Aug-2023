df916c62171cd43bc7732d4c244b2b2ac0a06350
Oğuz Karan <oguzkaran@csystem.org>
2023-10-14 12:51:28 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,20 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	
+	boolean türünden hiç bir türe ve hiç bir türden boolean türüne explicit dönüşüm yapılamaz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.print("İki sayı giriniz:");
-		double a = kb.nextDouble();
-		double b = kb.nextDouble();
-		double c;
-		
-		c = Math.sqrt(a) + Math.round(b);
-	
-		System.out.printf("c = %f%n", c);
+				
 	}
 }
