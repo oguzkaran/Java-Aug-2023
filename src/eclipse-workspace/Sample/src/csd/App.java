93691932ac7034ceed4c0a6fdf434929a05fb2d9
Oğuz Karan <oguzkaran@csystem.org>
2023-10-15 10:56:35 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,11 +1,27 @@
-/*----------------------------------------------------------------------------------------------------------------------	 
-	boolean türünden hiç bir türe ve hiç bir türden boolean türüne explicit dönüşüm yapılamaz
-----------------------------------------------------------------------------------------------------------------------*/
+
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-				
+		java.util.Scanner kb = new java.util.Scannner(System.in);
+		
+		
+		while (true) {
+			System.out.print("Birinci sayıyı giriniz:");
+			int a = Integer.parseInt(kb.nextLine());
+			
+			System.out.print("İkinci sayıyı giriniz:");
+			int b = Integer.parseInt(kb.nextLine());
+			
+			
+			
+			
+			if (a == 0 && b == 0)
+				break;
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
+
