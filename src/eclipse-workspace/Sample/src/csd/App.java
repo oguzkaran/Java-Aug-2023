9ef3fc850a3ede061336ffa405d279b28e7cefad
Oğuz Karan <oguzkaran@csystem.org>
2023-11-04 12:57:07 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,31 +1,54 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	 Aşağıdaki örnekte adres ataması yapılmadığı için, yani atanan "ŞEY" bir adres olmadığı için b'nin değişmesi a'yı 
-	 etkilemez
+	Anımsanacağı gibi Java'da 3 yerde atama yapılabilir. İstisna bir takım kurallar dışında atama işlemlerine ilişkin
+	kurallar hepsi için geçerlidir. Bir metodun parametre değişkeni bir referans olabilir. Bu durumda metot aynı türden
+	bir referans ile çağrılabilir. Bu durumda metot içerisinde argüman olarak geçilen referansa ilişkin nesneye 
+	erişilebilir. Bir metodun parametre geri dönüş değeri bir referans olabilir. Bu durumda return deyimine ilişkin
+	ifadenin de aynı türden bir referans olması gerekir. Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		int a = 10;
-		int b;
+		Date d = DateUtil.createDate(11, 7, 1983);
 		
-		b = a;
-		
-		System.out.printf("a = %d, b = %d%n", a, b);
-		
-		++b;
-		
-		System.out.printf("a = %d, b = %d%n", a, b);
+		DateUtil.printDate(d);		
+		DateUtil.changeDate(d, 6, 9, 2021);
+		DateUtil.printDate(d);
 	}
 }
 
-
-class Sample {
-	public int x;
-	public boolean y;
+class DateUtil {
+	public static Date createDate(int day, int month, int year)
+	{
+		//...
+		Date date = new Date();
+		
+		date.day = day;
+		date.month = month;
+		date.year = year;
+		
+		return date;
+	}
 	
+	public static void changeDate(Date date, int day, int month, int year)
+	{
+		//...
+		date.day = day;
+		date.month = month;
+		date.year = year;
+	}
+	
+	public static void printDate(Date date)
+	{
+		System.out.printf("%02d/%02d/%04d%n", date.day, date.month, date.year);
+	}
+}
+
+class Date {
+	public int day, month, year;
 	//...
 }
+
 
 
