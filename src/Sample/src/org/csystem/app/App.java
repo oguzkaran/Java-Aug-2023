d2a2c13ccee0f51dd21b866532e18cf56132d66c
Oğuz Karan <oguzkaran@csystem.org>
2023-12-24 10:26:38 +0300

    Update App.java

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,11 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Referans Dizileri (Reference Arrays): 
+	Referans Dizileri (Reference Arrays):
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String[] args)
 	{
-
+		
 	}
 }
