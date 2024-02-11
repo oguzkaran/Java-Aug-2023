afe80c7d9acf3c40ccde58a7653700b50b44e44f
Oğuz Karan <oguzkaran@csystem.org>
2024-02-11 10:32:28 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,101 +1,28 @@
 /*----------------------------------------------------------------------------------------------------------------------
-   Aşağıdaki demo örneği inceleyiniz
+   Object sınıfının equals isimli Object parametreli ve geri dönüş değeri boolean olan sanal bir metodu vardır. Bu metot
+   aynı türden iki nesne için mantıksal eşitlik karşılaştırmasında kullanılır. İki nesne için eşitlik kavramı söz konusuysa
+   equals metodu override edilir. Bu da Java'da bir convention'dır. Object sınıfının equals metodu klasik adres karşılaştırması
+   yapar. JavaSE ve üçüncü parti pek çok kütüphane içerisinde equals metodunu Object türü üzerinden çağıran sınıflar ve
+   dolayısıyla metotları bulunmaktadır. Örneğin ArrayList sınıfının indexOf ve lastIndexOf metotları arama işlemini
+   null adres araması yapılmıyorsa equals metodunu çağrırarak yaparlar. Bu durumda ArrayList içerisinde tutulan referansın
+   dinamik türüne göre override edilmişse, override edilen equals çağrılmış olur. Bu durumda ilgili tür için mantıksal
+   eşitlik karşılaştırması yapılmış olur.
+
+   Anahtar Notlar: Bir sınıfta equals metodu override ediliğinde, Object sınıfının hashCode metodu da override edilir veya
+   tersine hashCode metodu override edildiğinde equals metodu da override edilir. Yani bu iki metot bir sınıf içerisinde
+   ya ikisi birden override edilir ya da ikisi de override edilmez. hashCode metodu "Java ile Uygulama Geliştirme 1"
+   kursunda elde alınacaktır. Buradaki equals metodu override edilmiş sınıflarda override edilmeyecektir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.util.console.Console;
-import org.csystem.util.thread.ThreadUtil;
 
 import java.util.Random;
 
 class App {
     public static void main(String[] args)
     {
-        Factory factory = new Factory();
+        Random r = new Random();
 
-        while (true) {
-            A x = factory.create();
-
-            Util.doWork(x);
-            ThreadUtil.sleep(1000);
-        }
+        System.out.println(r);
     }
 }
 
-class Util {
-    public static void doWork(A a)
-    {
-        Console.writeLine("---------------------------------------------------------------");
-
-        Console.writeLine("Dynamic type:%s", a.getClass().getName());
-        a.foo();
-        Console.writeLine("---------------------------------------------------------------");
-    }
-}
-
-class Factory {
-    private final Random m_random = new Random();
-
-    public A create()
-    {
-        return switch (m_random.nextInt(5)) {
-            case 0 -> new B();
-            case 1 -> new C();
-            case 2 -> new D();
-            case 3 -> new E();
-            default -> new F();
-        };
-    }
-}
-
-class F extends D {
-    public void foo() //override
-    {
-        Console.writeLine("F.foo");
-        super.foo();
-    }
-}
-
-class E extends C {
-    public void foo() //override
-    {
-        Console.writeLine("E.foo");
-        super.foo();
-    }
-}
-
-class D extends A {
-    public void foo() //override
-    {
-        Console.writeLine("D.foo");
-        super.foo();
-    }
-}
-
-class C extends B {
-    public void foo() //override
-    {
-        Console.writeLine("C.foo");
-        super.foo();
-    }
-}
-
-class B extends A {
-    public void foo() //override
-    {
-        Console.writeLine("B.foo");
-        super.foo();
-    }
-}
-
-class A {
-    public void foo() //virtual method
-    {
-        Console.writeLine("A.foo");
-    }
-
-    public final void bar() //not a virtual method
-    {
-        Console.writeLine("A.bar");
-    }
-}
