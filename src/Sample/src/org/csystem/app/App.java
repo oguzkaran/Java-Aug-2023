dad4243dc55fd9b6c33ee12df1bcab5f62867fd0
Oğuz Karan <oguzkaran@csystem.org>
2024-03-02 14:25:15 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,12 +1,33 @@
-/*-----------------------------------------------------------------------------------------------------------------------
-    Generics: 
------------------------------------------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------------------------
+    Generic arayüzler:
+----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import org.csystem.util.console.Console;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 class App {
     public static void main(String[] args)
     {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
 
+        while (true) {
+            int n = Console.readInt("Bir sayı giriniz:");
+
+            if (n <= 0)
+                break;
+
+            numbers.add(random.nextInt(n));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int val : numbers)
+            sb.append(val).append(" ");
+
+        Console.writeLine(sb);
     }
 }
-
