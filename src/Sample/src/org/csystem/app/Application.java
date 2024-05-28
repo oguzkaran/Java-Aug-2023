c21c4f3d9252b3d3a6e28494d84bdf485636f18c
Oğuz Karan <oguzkaran@csystem.org>
2024-05-28 23:31:25 +0300

    Update

--- src/Sample/src/org/csystem/app/Application.java
+++ src/Sample/src/org/csystem/app/Application.java
@@ -1,21 +1,40 @@
 package org.csystem.app;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.csystem.util.console.CommandLineUtil.checkLengthEquals;
 import static org.csystem.util.console.Console.writeErrLine;
 import static org.csystem.util.console.Console.writeLine;
 
 public class Application {
+    private static void delete(Path path)
+    {
+        try {
+            Files.delete(path);
+            writeLine("%s deleted", path.getFileName());
+        }
+        catch (DirectoryNotEmptyException ignore) {
+            writeErrLine("Directory not empty:%s", path.getFileName());
+        }
+        catch (IOException ex) {
+            writeErrLine("IO Problem occurred:%s", ex.getMessage());
+        }
+    }
 
     public static void run(String[] args)
     {
         checkLengthEquals(1, args.length, "wrong number of arguments");
 
         try {
-            File f = new File(args[0]);
+            Path path = Path.of(args[0]);
+            if (Files.exists(path))
+                delete(path);
+            else
+                writeErrLine("No such file %s", path.getFileName());
 
-            writeLine(f.mkdirs() ? "%s created" : "%s exists", f.getName());
         }
         catch (Throwable ex) {
             writeErrLine("Problem occurred:%s", ex.getMessage());
