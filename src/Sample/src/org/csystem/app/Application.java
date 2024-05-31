9787475f70d6e5cf8a1ec4ba9dbdca828015365e
Oğuz Karan <oguzkaran@csystem.org>
2024-05-31 21:45:13 +0300

    Update

--- src/Sample/src/org/csystem/app/Application.java
+++ src/Sample/src/org/csystem/app/Application.java
@@ -1,40 +1,31 @@
 package org.csystem.app;
 
-import java.io.IOException;
-import java.nio.file.DirectoryNotEmptyException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 import static org.csystem.util.console.CommandLineUtil.checkLengthEquals;
 import static org.csystem.util.console.Console.writeErrLine;
 import static org.csystem.util.console.Console.writeLine;
 
 public class Application {
-    private static void delete(Path path)
-    {
-        try {
-            Files.delete(path);
-            writeLine("%s deleted", path.getFileName());
-        }
-        catch (DirectoryNotEmptyException ignore) {
-            writeErrLine("Directory not empty:%s", path.getFileName());
-        }
-        catch (IOException ex) {
-            writeErrLine("IO Problem occurred:%s", ex.getMessage());
-        }
-    }
 
     public static void run(String[] args)
     {
-        checkLengthEquals(1, args.length, "wrong number of arguments");
+        checkLengthEquals(2, args.length, "wrong number of arguments");
 
         try {
-            Path path = Path.of(args[0]);
-            if (Files.exists(path))
-                delete(path);
-            else
-                writeErrLine("No such file %s", path.getFileName());
+            Path src = Path.of(args[0]);
+            Path dest = Path.of(args[1]);
 
+            Files.move(src, dest);
+        }
+        catch (NoSuchFileException ex) {
+            writeLine("Source file not found!...");
+        }
+        catch (DirectoryNotEmptyException ex) {
+            writeLine("Target directory is not empty!...");
+        }
+        catch (InvalidPathException ex) {
+            writeErrLine("Invalid path: %s", ex.getMessage());
         }
         catch (Throwable ex) {
             writeErrLine("Problem occurred:%s", ex.getMessage());
